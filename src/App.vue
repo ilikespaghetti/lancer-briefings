04cd6e4887c45d33fc7470afe0e66200c6be5d60
ilikespaghetti <55904906+ilikespaghetti@users.noreply.github.com>
2022-07-13 18:08:14 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,62 +95,57 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Sterilization",
           "status": "start"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
+          "callsign": "Conscript",
+          "alias": "Jack Goodman",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "corpro": "SSC",
+          "frame": "Atlas",
+          "mech": "Balmung"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Drudge",
+          "alias": "Flint Aberdeen",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "corpro": "SSC",
+          "frame": "Emperor",
+          "mech": "Jerry"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Serpent",
+          "alias": "Simon Argyle",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "corpro": "SSC",
+          "frame": "Swallowtail Ranger",
+          "mech": "Barry"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "callsign": "Iodine",
+          "alias": "Roth Horesco",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
+          "corpro": "IPS-N",
+          "frame": "Caliban",
+          "mech": "Non-Refundable Deposit"
         },
         {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+        
         },
       ],
       "header": {
-        "planet": "Hercynia",
-        "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "planet": "CLASSIFIED",
+        "year": "5016u",
+        "system": "CLASSIFIED",
+        "gate": "CLASSIFIED",
+        "ring": "CLASSIFIED",
+        "headerTitle": "Smith-Shimano Corpro",
+        "headerSubtitle": ""You Only Need One"",
+        "subheaderTitle": "Materials Acquisition Team",
+        "subheaderSubtitle": "Mike-Oscar-Oscar-November-Lima-India-Golf-Hotel-Tango",
       },
       "options":{
         "eventsMarkdownPerMission": true
